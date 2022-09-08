bffadc3e8164effb61b47e9add37cb65243c03cf
GabrielVCastro <gabriel.gvc2020@gmail.com>
2022-09-08 20:46:19 -0300

    subindo teste de iframe e ajuste de borda lateral

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,4 +1,8 @@
 var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-  return new bootstrap.Tooltip(tooltipTriggerEl)
+return new bootstrap.Tooltip(tooltipTriggerEl)
 })
+
+function abrirModal(pessoa){
+  $("#modalIframe").modal('show')
+}
